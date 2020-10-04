ee9e3a65f4141938631a9eb7322653cfeb4abb5f
isaac-3 <isaacgc0596@gmail.com>
2020-10-04 16:13:45 -0500

    routes

--- frontend/src/components/Body.js
+++ frontend/src/components/Body.js
@@ -10,23 +10,18 @@ const Body = () => {
     const [cats, setCat] = useState([])
     let history = useHistory()
 
-    // useEffect(()=>{
-    //     fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
-    //     .then(res=>res.json())
-    //     .then(res => {
-    //         setCat(res.categories)
-    //     })
-    // },[])
+    useEffect(()=>{
+        fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+        .then(res=>res.json())
+        .then(res => {
+            setCat(res.categories)
+        })
+    },[])
 
-    // if(cats.length === 0){
-    //     return <p>loading</p>
-    // }
+    if(cats.length === 0){
+        return <p>loading</p>
+    }
 
-    // const seeCategory = () => {
-
-    // }
-
-    console.log(cats)
     return (
         <div className="body">
             <div className="body__search_box">
@@ -36,20 +31,20 @@ const Body = () => {
                 </IconButton>
             </div>
             <div className="body__cats">
-                {/* {cats.map(cat=>(
+                {cats.map(cat=>(
                     <Link to={`/category/${cat.strCategory}`}>
                         <div className="body__cats__indiv" key={cat.idCategory}>
                             <img src={cat.strCategoryThumb} alt="category"/>
                             <p>{cat.strCategory}</p>
                         </div>
                     </Link>
-                ))} */}
-                    <Link to={`/category/${'Beef'}`}>
+                ))}
+                    {/* <Link to={`/category/${'Beef'}`}>
                         <div className="body__cats__indiv">
                             <img src="https://www.helpguide.org/wp-content/uploads/fast-foods-candy-cookies-pastries-768.jpg" alt="category"/>
                             <p>yum</p>
                         </div>
-                    </Link>
+                    </Link> */}
             </div>
         </div>
     );
